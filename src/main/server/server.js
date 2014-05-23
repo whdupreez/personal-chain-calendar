5400235d69e7cbcdd84a01b957184a9cd308cf94
whdupreez <willydupreez@gmail.com>
2014-05-22 21:11:54 -0600

    Switching database to PostgreSQL

--- src/main/server/server.js
+++ src/main/server/server.js
@@ -3,6 +3,9 @@ var express = require('express');
 
 // require('nw.gui').App.dataPath in future?
 var db = new Datastore({ filename: 'target-grunt/datafile.nedb', autoload: true });
+var pg = require('pg');
+var conString = "postgres://username:password@localhost/database";
+
 var app = express();
 
 app.use(require('body-parser')());      // to support JSON-encoded bodies
@@ -30,13 +33,30 @@ app.get('/', function(req, res) {
 });
 
 app.get('/calendar/default', function(req, res) {
-	db.find({ name: 'default' }, function(err, docs) {
-		if (docs.length === 0) {
-			res.send(404, 'No calendar with name "default".');
-		} else {
-			res.send(docs[0]);
+
+	pg.connect(conString, function(err, client, done) {
+		if (err) {
+			return console.error('error fetching client from pool', err);
 		}
+		client.query('SELECT $1::int AS number', [ '1' ], function(err, result) {
+			// call `done()` to release the client back to the pool
+			done();
+
+			if (err) {
+				return console.error('error running query', err);
+			}
+			console.log(result.rows[0].number);
+			// output: 1
+		});
 	});
+
+// db.find({ name: 'default' }, function(err, docs) {
+//		if (docs.length === 0) {
+			res.send(404, 'No calendar with name "default".');
+//		} else {
+//			res.send(docs[0]);
+//		}
+//	});
 //	res.send({
 //		name : 'default',
 //		dates : {
